0c4f1b0f199370348e1cde7819d18930ad8b4178
sachin <msgtosachin@gmail.com>
2022-10-08 21:31:14 +0530

    Added code for alphanumeric conversion

--- Generators1.py
+++ Generators1.py
@@ -6,19 +6,24 @@ Iterator - __next__()
 """
 
 # User Defined Generator
+SearchResult = []
 def gen(n):
-    for i in range(n):
-        yield i
-
-g=gen(3)
+    for i in range(n):    
+        if i%2==0:    
+            yield i    
+        
+        
+g=gen(10)
 print(g)
-print(g.__next__())
-print(g.__next__())
-print(g.__next__())
+print(SearchResult)
+# print(g.__next__())
+# print(g.__next__())
+# print(g.__next__())
 print("*********** Before print **********")
-for i in g:
-    print(i)
+ans=[]
+for i in g:    
+    ans.append(i)
 print("*********** After print ***********")
-
-print(g.__next__()) # This will give error there is no value after 2 in the range and sinc it only iterates once, it will not go back to first index
+print(ans)
+# print(g.__next__()) # This will give error there is no value after 2 in the range and sinc it only iterates once, it will not go back to first index
 
