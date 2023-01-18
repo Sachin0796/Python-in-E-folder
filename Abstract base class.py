47c1a2ceb64b5e015d06afea58a0b380b89fc27e
sachin <msgtosachin@gmail.com>
2023-01-19 02:45:37 +0530

    Added multithreading and updated ABC

--- Abstract base class.py
+++ Abstract base class.py
@@ -1,9 +1,9 @@
 # from abc import ABC, abstractmethod   # Syntax A line 1, This works in v3.4 or above
 # class Shape(ABC):                     # Syntax A line 2
-# ABCMeta is the class which can ask its derived classes to mandatorily implement some methods
 # We cannot instantiate the abstract base class
 from abc import ABCMeta, abstractmethod # Syntax B line 1
 class Shape(metaclass=ABCMeta):         # Syntax B line 2
+# metaclass=ABCMeta or ABC is the class which can ask its derived classes to mandatorily implement some methods
     @abstractmethod # here we are declaring this function as abstract so that it has to be implemented by all the derived class.
     def printArea(self):
         return 0
