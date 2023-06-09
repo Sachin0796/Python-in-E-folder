38178b8532e08a799188612e8dfd19006842e9c9
sachin <msgtosachin@gmail.com>
2023-06-09 15:47:44 +0530

    Added Database file

---
+++ connectDatabase.py
@@ -0,0 +1,25 @@
+import cx_Oracle
+
+def InsertRecord():
+    con = cx_Oracle.connect('HR/hr123@localhost:1521/orcl1')
+    # print(con.version)
+    cursor = con.cursor()
+    cursor.execute("insert into employees(EMPLOYEE_ID, LAST_NAME, EMAIL, HIRE_DATE, JOB_ID) values (10, 'khanna', 'abc', '20-AUG-07', 'AD_PRES')")
+    con.commit()    
+    print("Record inserted successfully !")
+    cursor.close()
+    con.close()
+
+def printRecords():
+    con = cx_Oracle.connect('HR/hr123@localhost:1521/orcl1')
+    # print(con.version)
+    cursor = con.cursor()
+    cursor.execute('select salary from employees')
+    rows = cursor.fetchmany(10)
+    print(rows)
+    cursor.close()
+    con.close()
+
+
+# InsertRecord()
+printRecords()
